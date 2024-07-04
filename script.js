80bbf3cb3348d467034d80792c2bdd5c5a1dcdee
Mikiyas6 <Mikitedi9@gmail.com>
2024-07-04 14:38:14 +0300

    In this section, I tested the map method of the array object.

--- script.js
+++ script.js
@@ -86,6 +86,17 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(function (movement) {
+  return movement * eurToUsd;
+});
+
+const movementUSD = movements.map(movement => movement * eurToUsd);
+console.log(movementUSD);
