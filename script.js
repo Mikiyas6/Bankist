4f7082553200fed759e6025ad1fdf3e6aa37b94f
Mikiyas6 <Mikitedi9@gmail.com>
2024-07-13 18:08:01 +0300

    final commit

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Mekdes Kebede',
+  owner: 'Birkity Yishak',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
